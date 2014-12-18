561f423361e6bceac7780e44afa171a866fd7fbb
jrozra200 <rozran00@gmail.com>
2014-12-18 08:37:21 -0500

    plot1 is complete

--- plot1.R
+++ plot1.R
@@ -1,3 +1,13 @@
 plot1 <- function() {
+        library(plyr)
         
+        NEI <- readRDS("./exdata-data-NEI_data/summarySCC_PM25.rds")
+        
+        overalltrend <- ddply(NEI, .(year), summarize, sum = sum(Emissions))
+        
+        png("plot1.png")
+        plot(overalltrend$year, overalltrend$sum, pch = ".", xlab = "YEAR", ylab = "Overall Emissions")
+        lines(overalltrend$year, overalltrend$sum)
+        title(main = "Overall Emissions by Year")
+        dev.off()
 }
